16e856d632ee49f6bb5538da8de477608f8dc6fa
Andrei Onu <onu.andr@gmail.com>
2013-11-02 18:28:31 +0200

    reverting change

--- frontend/public_html/js/modules/settings.js
+++ frontend/public_html/js/modules/settings.js
@@ -3,6 +3,7 @@ define({
     authenticationAge: 10,
     getGoogleOpenIdLink: function() {
         var baseUrl = 'https://www.google.com/accounts/o8/ud?openid.ns=http://specs.openid.net/auth/2.0&openid.ns.pape=http://specs.openid.net/extensions/pape/1.0&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.ui.ns=http://specs.openid.net/extensions/ui/1.0&openid.ui.mode=popup&openid.ui.icon=true&openid.ns.ax=http://openid.net/srv/ax/1.0&openid.ax.mode=fetch_request&openid.ax.type.email=http://axschema.org/contact/email&openid.ax.required=email';
-        return baseUrl + '&openid.ns.max_auth_age=' + this.authenticationAge + '&openid.return_to=' + this.domain + '&openid.realm=' + this.domain;
+        var webAppDomain = "http://localhost:8383/doit/login.html";
+        return baseUrl + '&openid.ns.max_auth_age=' + this.authenticationAge + '&openid.return_to=' + webAppDomain + '&openid.realm=' + webAppDomain;
     }
 });
