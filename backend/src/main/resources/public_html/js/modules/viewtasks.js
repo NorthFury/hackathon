18ab32339f5dc1acda48031094a1f3ef727c3a83
Ariel Chelsau <arielchelsau@github.com>
2013-11-03 09:50:39 +0200

    view tasks data

--- backend/src/main/resources/public_html/js/modules/viewtasks.js
+++ backend/src/main/resources/public_html/js/modules/viewtasks.js
@@ -1,68 +1,44 @@
 define(['jquery', 'underscore', 'settings', 'text!task.html!strip'], function($, _, settings, taskTemplateString) {
     var taskTemplate = _.template(taskTemplateString);
+    var tasksArray = [];
 
-    var buildDoItOnClick = function(userId, taskId, tasksContainer) {
-        return function() {
-            $.ajax({
-                url: 'account/' + userId + '/task/' + taskId + '/markDone',
-                type: 'PUT',
-                dataType: 'json',
-                success: function(data) {
-                    console.log(data);
-                    updateTasksContainer(tasksContainer);
-                }
-            });
-        };
-    };
-
-    var updateTasksContainer = function(tasksContainer) {
-        tasksContainer.html('');
-        tasksContainer.append('<thead><tr><th>Activity</th><th>Task</th><th>Importance</th><th>Done</th><th>Achievement</th></tr></thead>');
-        var tbodyContainer = $('<tbody/>');
-        tasksContainer.append(tbodyContainer);
-
+    return function(container) {
         var userId = settings.login.account.id;
+        var tasksContainer = $('<div/>');
 
         $.getJSON('/account/' + userId + '/tasks', function(data) {
             data.push({
-                task: {
-                    id: 1,
-                    name: "Task 1"
-                },
+                name: "Task 1",
                 activity: {
                     id: 1,
                     name: "Sports"
-                },
-                done: true,
-                importance: 5
+                }
             });
             data.push({
-                task: {
-                    id: 1,
-                    name: "Task 1"
-                },
+                name: "Task 2",
                 activity: {
                     id: 1,
-                    name: "Sports"
-                },
-                done: false,
-                importance: 4
+                    name: "House"
+                }
             });
 
             for (var i = 0; i < data.length; i++) {
-                var taskElement = $(taskTemplate(data[i]));
-
-                taskElement.find('.doIt').on('click', buildDoItOnClick(userId, data[i].id));
-                tbodyContainer.append(taskElement);
+                var taskObject = {
+                    activity: data[i].activity.name,
+                    name: data[i].name
+                };
+                
+                tasksArray.push(taskObject);
+                
+                // tasksContainer.append(taskTemplate(taskObject));
             }
+            
+            var taskListObject = {
+                    taskList: tasksArray
+                };
+            
+            tasksContainer.append(taskTemplate(taskListObject));
         });
-    };
-
-    return function(container) {
-        var tasksContainer = $('<table class="table"/>');
-
-        updateTasksContainer(tasksContainer);
-
         container.html('');
         container.append(tasksContainer);
     };
